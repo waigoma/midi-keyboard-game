6add47dfaf917113ab7cceaa5796fb58e75c7af6
waigoma <waichi0302@gmail.com>
2022-10-13 22:32:20 +0900

    update MidiPlayerBehavior.cs

--- Assets/MidiKeyGame/Scripts/MidiPlayerBehavior.cs
+++ Assets/MidiKeyGame/Scripts/MidiPlayerBehavior.cs
@@ -1,7 +1,32 @@
-﻿namespace MidiKeyGame.Scripts
+﻿using System;
+using MidiPlayerTK;
+using UnityEngine;
+
+namespace MidiKeyGame.Scripts
 {
-    public class MidiPlayerBehavior
+    public class MidiPlayerBehavior : MonoBehaviour
     {
+        [SerializeField] private int index = 90;
         
+        private MidiPlayerController _midiPlayerController;
+        
+        private void Start()
+        {
+            var mfl = FindObjectOfType<MidiFileLoader>();
+            var mfp = FindObjectOfType<MidiFilePlayer>();
+            
+            _midiPlayerController = new MidiPlayerController(mfl, mfp);
+        }
+
+        public void PlayIndex()
+        {
+            MidiPlayerController.SetIndex(index);
+            _midiPlayerController.PlayIndexMidi();
+        }
+        
+        public void SetIndex(int idx)
+        {
+            index = idx;
+        }
     }
 }
