5ead741af128aa9a28f00ae530c6fd0c27e70c3c
waigoma <waichi0302@gmail.com>
2022-10-13 22:37:55 +0900

    add TMP, Button
    Text Mesh Pro のインポート
    ボタン配置

--- Assets/MidiKeyGame/Scripts/Button/PlayButton.cs
+++ Assets/MidiKeyGame/Scripts/Button/PlayButton.cs
@@ -1,7 +1,20 @@
-﻿namespace MidiKeyGame.Scripts.Button
+﻿using System;
+using UnityEngine;
+
+namespace MidiKeyGame.Scripts.Button
 {
-    public class PlayButton
+    public class PlayButton : MonoBehaviour
     {
+        private MidiPlayerBehavior _midiPlayerBehavior;
         
+        private void Start()
+        {
+            _midiPlayerBehavior = GetComponent<MidiPlayerBehavior>();
+        }
+
+        public void OnClick()
+        {
+            _midiPlayerBehavior.PlayIndex();
+        }
     }
 }
