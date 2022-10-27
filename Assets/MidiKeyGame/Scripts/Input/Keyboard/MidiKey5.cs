861aa7ab4c842a004e0a51f17de2e8f3c47bd215
waigoma <waichi0302@gmail.com>
2022-10-28 00:36:44 +0900

    Create MidiKey5.cs

---
+++ Assets/MidiKeyGame/Scripts/Input/Keyboard/MidiKey5.cs
@@ -0,0 +1,28 @@
+﻿namespace MidiKeyGame.Scripts.Input.Keyboard
+{
+    public class MidiKey5 : MidiKey
+    {
+        public MidiKey5(MidiKeyInputAction inputAction)
+        {
+            this.inputAction = inputAction;
+            octave = 5;
+        }
+        
+        public void Initialize()
+        {
+            var mkm = inputAction.MidiKeyMap5;
+            mkm.C.performed += COnPerformed;
+            mkm.Cs.performed += CsOnPerformed;
+            mkm.D.performed += DOnPerformed;
+            mkm.Ds.performed += DsOnPerformed;
+            mkm.E.performed += EOnPerformed;
+            mkm.F.performed += FOnPerformed;
+            mkm.Fs.performed += FsOnPerformed;
+            mkm.G.performed += GOnPerformed;
+            mkm.Gs.performed += GsOnPerformed;
+            mkm.A.performed += AOnPerformed;
+            mkm.As.performed += AsOnPerformed;
+            mkm.B.performed += BOnPerformed;
+        }
+    }
+}
