f5fcc2ca76e5751b35945fb4a4811ac9d0a8391d
waigoma <48357625+waigoma@users.noreply.github.com>
2022-10-28 14:37:46 +0900

    create MidiKey0.cs

---
+++ Assets/MidiKeyGame/Scripts/Input/Keyboard/MidiKey0.cs
@@ -0,0 +1,19 @@
+﻿namespace MidiKeyGame.Scripts.Input.Keyboard
+{
+    public class MidiKey0 : MidiKey
+    {
+        public MidiKey0(MidiKeyInputAction inputAction)
+        {
+            this.inputAction = inputAction;
+            octave = 0;
+        }
+        
+        public override void Initialize()
+        {
+            var mkm = inputAction.MidiKeyMap0;
+            mkm.A.performed += AOnPerformed;
+            mkm.As.performed += AsOnPerformed;
+            mkm.B.performed += BOnPerformed;
+        }
+    }
+}
