dafbbbc957d1639653ba897ac3afe7f6db3ba1ce
waigoma <48357625+waigoma@users.noreply.github.com>
2022-10-28 14:36:02 +0900

    create MidiKey8.cs

---
+++ Assets/MidiKeyGame/Scripts/Input/Keyboard/MidiKey8.cs
@@ -0,0 +1,17 @@
+﻿namespace MidiKeyGame.Scripts.Input.Keyboard
+{
+    public class MidiKey8 : MidiKey
+    {
+        public MidiKey8(MidiKeyInputAction inputAction)
+        {
+            this.inputAction = inputAction;
+            octave = 8;
+        }
+        
+        public override void Initialize()
+        {
+            var mkm = inputAction.MidiKeyMap8;
+            mkm.C.performed += COnPerformed;
+        }
+    }
+}
