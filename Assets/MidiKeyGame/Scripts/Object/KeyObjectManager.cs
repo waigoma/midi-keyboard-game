f585bdbf85b821567472c121f9f39a2110f90dd9
waigoma <waichi0302@gmail.com>
2022-11-10 22:14:04 +0900

    Create KeyObjectManager

---
+++ Assets/MidiKeyGame/Scripts/Object/KeyObjectManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace MidiKeyGame.Scripts.Object
+{
+    public static class KeyObjectManager
+    {
+        public static readonly Dictionary<string, GameObject> Keys0 = new();
+        public static readonly Dictionary<string, GameObject> Keys1 = new();
+        public static readonly Dictionary<string, GameObject> Keys2 = new();
+        public static readonly Dictionary<string, GameObject> Keys3 = new();
+        public static readonly Dictionary<string, GameObject> Keys4 = new();
+        public static readonly Dictionary<string, GameObject> Keys5 = new();
+        public static readonly Dictionary<string, GameObject> Keys6 = new();
+        public static readonly Dictionary<string, GameObject> Keys7 = new();
+        public static readonly Dictionary<string, GameObject> Keys8 = new();
+    }
+}
