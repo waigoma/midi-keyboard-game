52c0646e0846a09de0ae876969ed2c1cdaa64368
waigoma <waichi0302@gmail.com>
2022-11-10 21:37:26 +0900

    move file

---
+++ Assets/MidiKeyGame/Scripts/Object/KeyObjectBehavior.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+
+namespace MidiKeyGame.Scripts
+{
+    public class KeyObjectBehavior : MonoBehaviour
+    {
+        [SerializeField] private GameObject a0;
+        [SerializeField] private GameObject as0;
+        [SerializeField] private GameObject b0;
+        
+        [SerializeField] private GameObject c1;
+        [SerializeField] private GameObject cs1;
+        [SerializeField] private GameObject d1;
+        [SerializeField] private GameObject ds1;
+        [SerializeField] private GameObject e1;
+        [SerializeField] private GameObject f1;
+        [SerializeField] private GameObject fs1;
+        [SerializeField] private GameObject g1;
+        [SerializeField] private GameObject gs1;
+        [SerializeField] private GameObject a1;
+        [SerializeField] private GameObject as1;
+        [SerializeField] private GameObject b1;
+        
+        [SerializeField] private GameObject c2;
+        [SerializeField] private GameObject cs2;
+        [SerializeField] private GameObject d2;
+        [SerializeField] private GameObject ds2;
+        [SerializeField] private GameObject e2;
+        [SerializeField] private GameObject f2;
+        [SerializeField] private GameObject fs2;
+        [SerializeField] private GameObject g2;
+        [SerializeField] private GameObject gs2;
+        [SerializeField] private GameObject a2;
+        [SerializeField] private GameObject as2;
+        [SerializeField] private GameObject b2;
+        
+        [SerializeField] private GameObject c3;
+        [SerializeField] private GameObject cs3;
+        [SerializeField] private GameObject d3;
+        [SerializeField] private GameObject ds3;
+        [SerializeField] private GameObject e3;
+        [SerializeField] private GameObject f3;
+        [SerializeField] private GameObject fs3;
+        [SerializeField] private GameObject g3;
+        [SerializeField] private GameObject gs3;
+        [SerializeField] private GameObject a3;
+        [SerializeField] private GameObject as3;
+        [SerializeField] private GameObject b3;
+        
+        [SerializeField] private GameObject c4;
+        [SerializeField] private GameObject cs4;
+        [SerializeField] private GameObject d4;
+        [SerializeField] private GameObject ds4;
+        [SerializeField] private GameObject e4;
+        [SerializeField] private GameObject f4;
+        [SerializeField] private GameObject fs4;
+        [SerializeField] private GameObject g4;
+        [SerializeField] private GameObject gs4;
+        [SerializeField] private GameObject a4;
+        [SerializeField] private GameObject as4;
+        [SerializeField] private GameObject b4;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
