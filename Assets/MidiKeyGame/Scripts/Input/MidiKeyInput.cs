cd49a6739497e091be831769b6748e1c3d88e17d
waigoma <waichi0302@gmail.com>
2022-11-17 23:11:46 +0900

    add MeltySynth package

--- Assets/MidiKeyGame/Scripts/Input/MidiKeyInput.cs
+++ Assets/MidiKeyGame/Scripts/Input/MidiKeyInput.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using MeltySynth;
 using MidiKeyGame.Scripts.Input.Keyboard;
 using UnityEngine;
 
@@ -7,7 +10,7 @@ namespace MidiKeyGame.Scripts.Input
     public class MidiKeyInput : MonoBehaviour
     {
         private MidiKeyInputAction _inputAction;
-        
+
         private void Start()
         {
             _inputAction = new MidiKeyInputAction();
@@ -28,6 +31,17 @@ namespace MidiKeyGame.Scripts.Input
             foreach (var mk in mkList) mk.Initialize();
             
             _inputAction.Enable();
+            
+            var synth = new Synthesizer("./Assets/MidiKeyGame/SoundFont/Equinox_Grand_Pianos.sf2", 44100);
+            synth.NoteOn(0, 60, 50);
+            
+            var left = new float[3*44100];
+            var right = new float[3*44100];
+            
+            synth.Render(left, right);
+            
+            synth.NoteOff(0, 60);
+            Debug.Log($"left: {left}, right: {right}");
         }
     }
 
