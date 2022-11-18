9294abdf0804aeca0211683b2a4b0099742e57f3
waigoma <48357625+waigoma@users.noreply.github.com>
2022-11-18 15:13:35 +0900

    Update KeyRealtimeAudio.cs

--- Assets/MidiKeyGame/Scripts/Audio/KeyRealtimeAudio.cs
+++ Assets/MidiKeyGame/Scripts/Audio/KeyRealtimeAudio.cs
@@ -8,9 +8,19 @@ namespace MidiKeyGame.Scripts.Audio
         [SerializeField] private AudioSource audioSource;
         private readonly Synthesizer _synthesizer = SynthManager.Synth;
         
+        private const float FPS = 60;
+        
         private void Update()
         {
+            var len = (int) (1 / FPS * _synthesizer.SampleRate);
+            var left = new float[len];
+            var right = new float[len];
+            _synthesizer.Render(left, right);
             
+            var clip = AudioClip.Create("test", len, 2, _synthesizer.SampleRate, false);
+            clip.SetData(left, 0);
+            
+            audioSource.PlayOneShot(clip);
         }
     }
 }
