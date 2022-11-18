266c46445a746ed3e9a193a7d9b611a2db4949c5
waigoma <48357625+waigoma@users.noreply.github.com>
2022-11-18 14:58:21 +0900

    Create SynthManager.cs

---
+++ Assets/MidiKeyGame/Scripts/Audio/SynthManager.cs
@@ -0,0 +1,24 @@
+﻿using MeltySynth;
+
+namespace MidiKeyGame.Scripts.Audio
+{
+    public static class SynthManager
+    {
+        private const string SoundFont = "./Assets/MidiKeyGame/SoundFont/Equinox_Grand_Pianos.sf2";
+        private const int SampleRate = 44100;
+        private const int Velocity = 100;
+        private const int Length = 10;
+        
+        public static readonly Synthesizer Synth = new (SoundFont, SampleRate);
+        
+        public static void NoteOn(int note)
+        {
+            Synth.NoteOn(0, note, Velocity);
+        }
+        
+        public static void NoteOff(int note)
+        {
+            Synth.NoteOff(0, note);
+        }
+    }
+}
