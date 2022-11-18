e4abd42d3e5e0050e635574c6b6203e490d16cf8
waigoma <48357625+waigoma@users.noreply.github.com>
2022-11-18 14:18:43 +0900

    Update KeyMaterial.cs

--- Assets/MidiKeyGame/Scripts/Object/KeyMaterial.cs
+++ Assets/MidiKeyGame/Scripts/Object/KeyMaterial.cs
@@ -1,7 +1,30 @@
-﻿namespace MidiKeyGame.Scripts.Object
+﻿using UnityEngine;
+
+namespace MidiKeyGame.Scripts.Object
 {
     public class KeyMaterial
     {
+        public void Pressed(GameObject gameObject)
+        {
+            gameObject.GetComponent<Renderer>().material = KeyObjectManager.WhiteKeyPressedMaterial;
+        }
         
+        public void Released(GameObject gameObject)
+        {
+            var objName = gameObject.name;
+            
+            if (objName.Contains("#")) BlackKeyReleased(gameObject);
+            else WhiteKeyReleased(gameObject);
+        }
+        
+        private void WhiteKeyReleased(GameObject gameObject)
+        {
+            gameObject.GetComponent<Renderer>().material = KeyObjectManager.WhiteKeyNormalMaterial;
+        }
+        
+        private void BlackKeyReleased(GameObject gameObject)
+        {
+            gameObject.GetComponent<Renderer>().material = KeyObjectManager.BlackKeyNormalMaterial;
+        }
     }
 }
