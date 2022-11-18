988ab991a58e5138236c3ea0756afba66f369f73
waigoma <48357625+waigoma@users.noreply.github.com>
2022-11-18 14:58:31 +0900

    Create KeyRealtimeAudio.cs

---
+++ Assets/MidiKeyGame/Scripts/Audio/KeyRealtimeAudio.cs
@@ -0,0 +1,16 @@
+﻿using MeltySynth;
+using UnityEngine;
+
+namespace MidiKeyGame.Scripts.Audio
+{
+    public class KeyRealtimeAudio : MonoBehaviour
+    {
+        [SerializeField] private AudioSource audioSource;
+        private readonly Synthesizer _synthesizer = SynthManager.Synth;
+        
+        private void Update()
+        {
+            
+        }
+    }
+}
